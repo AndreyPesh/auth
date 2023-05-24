fc26b932b1d1af8e55d7b29ebb7236cf96e5c93e
Andrey Peshko <coursestud@mail.ru>
2023-05-24 11:33:51 +0300

    refactor: add desktop navigation

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -2,9 +2,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { useSession, getProviders } from 'next-auth/react';
 import { connectDB } from '@/utils/database';
 import MobileNav from './MobileNav';
+import DesktopNav from './DesktopNav';
 
 export type ProvidersType = Awaited<ReturnType<typeof getProviders>>;
 
@@ -23,10 +24,6 @@ const Nav = () => {
     setProvidersToState();
   }, []);
 
-  useEffect(() => {
-    console.log('session ', session, 'providers: ', providers);
-  }, [session, providers]);
-
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -41,49 +38,7 @@ const Nav = () => {
       </Link>
 
       {/* Desktop Navigation */}
-      <div className="sm:flex hidden">
-        {session?.user ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">
-              Create Post
-            </Link>
-
-            <button
-              type="button"
-              onClick={() => signOut()}
-              className="outline_btn"
-            >
-              Sign Out
-            </button>
-
-            <Link href="/profile">
-              <Image
-                src={'/assets/images/logo.svg'}
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-            </Link>
-          </div>
-        ) : (
-          <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => {
-                    signIn(provider.id);
-                  }}
-                  className="black_btn"
-                >
-                  Sign in
-                </button>
-              ))}
-          </>
-        )}
-      </div>
+      <DesktopNav providers={providers} />
 
       {/* Mobile Navigation */}
       <MobileNav providers={providers} />

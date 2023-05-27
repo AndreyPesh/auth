47b2d55634a77279597c4b59d976be738845a1d3
Andrei <coursestud@mail.ru>
2023-05-27 14:25:29 +0300

    feat: add categories icons

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Search from './Search';
 import UserMenu from './UserMenu';
 import { FC } from 'react';
 import { SafeUser } from '@/app/types';
+import Categories from './Categories';
 
 interface NavbarProps {
   currentUser: SafeUser | null;
@@ -23,6 +24,7 @@ const Navbar: FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };

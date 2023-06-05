fce031867a1f4fe6f8df0721bc8f6872e4cb9dcb
Andrey Peshko <coursestud@mail.ru>
2023-06-05 16:41:39 +0300

    feat: add individual listing

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -5,6 +5,7 @@ import { SafeUser } from '@/app/types';
 import useCountries from '@/app/hooks/useCountries';
 import Heading from '../Heading';
 import Image from 'next/image';
+import HeartButton from '../HeartButton';
 
 interface ListingHeadProps {
   title: string;
@@ -36,6 +37,9 @@ const ListingHead: FC<ListingHeadProps> = ({
           fill
           className="object-cover w-full"
         />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
       </div>
     </>
   );

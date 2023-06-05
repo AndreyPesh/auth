57a1bec9ed75acff7a1ba09c5d832bad3a180ab5
Andrey Peshko <coursestud@mail.ru>
2023-06-05 14:51:29 +0300

    feat: add toggle favorite listing

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 import { FC } from 'react';
 import { SafeUser } from '../types';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '../hooks/useFavorite';
 
 interface HeartButtonProps {
   listingId: string;
@@ -9,15 +10,23 @@ interface HeartButtonProps {
 }
 
 const HeartButton: FC<HeartButtonProps> = ({ listingId, currentUser }) => {
-  const hasFavorite = true;
-  const toggleFavorite = () => {};
+  const { hasFavorite, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
   return (
     <div
       onClick={toggleFavorite}
       className="relative hover:opacity-80 transition cursor-pointer"
     >
-      <AiOutlineHeart size={28} className='fill-yellow absolute -top-[2px] -right-[2px]' />
-      <AiFillHeart size={24} className={hasFavorite ? 'fill-rose-500' : 'fill-neutral-500/70'} />
+      <AiOutlineHeart
+        size={28}
+        className="fill-yellow absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorite ? 'fill-rose-500' : 'fill-neutral-500/70'}
+      />
     </div>
   );
 };
